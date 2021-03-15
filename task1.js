e18f5568e1e03b5afb93a07022ed06a6368684a6
Elizaveta Tomaeva <elizaveta_tomaeva@epam.com>
2021-03-15 22:21:16 +0300

    task 1 fixed

---
+++ task1.js
@@ -0,0 +1,20 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+});
+
+function reverseStr(inputStr) {
+    return inputStr.split("").reverse().join("");
+}
+
+function ask() {
+    rl.question('', (answer) => {
+        console.log(`${reverseStr(answer)}`);
+        ask();
+      });
+}
+
+ask();
