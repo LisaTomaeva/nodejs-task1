c8cdc3c10f4a135756948cfba6a55a7c818807cb
Elizaveta Tomaeva <elizaveta_tomaeva@epam.com>
2021-03-17 23:16:31 +0300

    task2 added & fixes

---
+++ task2.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path'); 
+
+const csvFilePath='./csv/books.csv';
+const txtFile='./books.txt';
+const csv=require('csvtojson');
+
+try {
+	if (fs.statSync(txtFile)) { 
+		fs.unlinkSync(txtFile)
+	  } 
+} catch(err) {
+	console.log(err)
+}
+
+
+csv().fromFile(csvFilePath)
+.on('error', (err) => {
+	console.log(err)
+})
+.on('data',(l)=>{
+	const jsonStr= l.toString('utf8')
+    console.log(jsonStr)
+	try {
+		fs.appendFileSync(txtFile, l.toString() + "\n")
+	} catch(e) {
+		console.log(e)
+	}
+})
